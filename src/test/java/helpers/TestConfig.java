357b02f538e25df04ef4bdda31d113e533567635
Schnzq <michal.m.mozdzen@gmail.com>
2021-06-24 17:14:46 +0200

    initSpecification method moved to TestConfig.java

--- src/test/java/helpers/TestConfig.java
+++ src/test/java/helpers/TestConfig.java
@@ -1,5 +1,8 @@
 package helpers;
 
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 
@@ -9,8 +12,19 @@ public class TestConfig {
     public static final RequestSpecification MULTIPART_SPEC;
 
     static {
-        JSON_SPEC = TestHelper.initSpecification(ContentType.JSON,ContentType.JSON);
-        URLENC_SPEC = TestHelper.initSpecification(ContentType.URLENC,ContentType.JSON);
-        MULTIPART_SPEC = TestHelper.initSpecification(ContentType.MULTIPART,ContentType.JSON);
+        JSON_SPEC = initSpecification(ContentType.JSON, ContentType.JSON);
+        URLENC_SPEC = initSpecification(ContentType.URLENC, ContentType.JSON);
+        MULTIPART_SPEC = initSpecification(ContentType.MULTIPART, ContentType.JSON);
     }
+
+    public static RequestSpecification initSpecification(ContentType contentType, ContentType accept) {
+        return new RequestSpecBuilder()
+                .setContentType(contentType)
+                .setAccept(accept)
+                .setBaseUri("https://petstore.swagger.io/v2/")
+                .addFilter(new ResponseLoggingFilter())
+                .addFilter(new RequestLoggingFilter())
+                .build();
+    }
+
 }
--- src/test/java/helpers/TestHelper.java
+++ src/test/java/helpers/TestHelper.java
@@ -1,29 +1,13 @@
 package helpers;
 
-import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.filter.log.RequestLoggingFilter;
-import io.restassured.filter.log.ResponseLoggingFilter;
-import io.restassured.http.ContentType;
 import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
 import models.Pet;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class TestHelper {
-
-    public static RequestSpecification initSpecification(ContentType contentType, ContentType accept) {
-        return new RequestSpecBuilder()
-                .setContentType(contentType)
-                .setAccept(accept)
-                .setBaseUri("https://petstore.swagger.io/v2/")
-                .addFilter(new ResponseLoggingFilter())
-                .addFilter(new RequestLoggingFilter())
-                .build();
-    }
 
     public static void assertEqualPet(Pet requestBody, Pet responseBody, String... ignoredFields) {
         assertThat(responseBody)
